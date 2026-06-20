e0999c7d8a071d3e23d3aa40de0798797e3599bf
Khoa Tran <rosekai785@gmail.com>
2026-06-20 10:38:30 +0700

    Delete sales-platform directory

--- sales-platform/backend/src/main/java/com/company/sales_management/repository/BrandRepository.java
+++
@@ -1,9 +0,0 @@
-package com.company.sales_management.repository;
-
-import com.company.sales_management.entity.Brand;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface BrandRepository extends JpaRepository<Brand, Long> {
-    List<Brand> findByNameContainingIgnoreCaseOrderByIdDesc(String name);
-}
--- sales-platform/backend/src/main/java/com/company/sales_management/repository/StoreSettingsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.sales_management.repository;
-
-import com.company.sales_management.entity.StoreSettings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreSettingsRepository extends JpaRepository<StoreSettings, Long> {
-}
